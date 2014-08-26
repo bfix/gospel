e220390b28f6a3e221c7f4c6caa45f2a8ea078bf
Bernd Fix <brf@hoi-polloi.org>
2014-08-26 16:07:45 +0200

    Parser/handler for mail messages (plain, encrypted, signed).

--- network/smtp.go
+++ network/smtp.go
@@ -38,13 +38,17 @@ import (
 	"crypto/tls"
 	"errors"
 	"fmt"
+	"github.com/bfix/gospel/crypto"
 	"github.com/bfix/gospel/logger"
 	"io"
+	"io/ioutil"
 	"mime/multipart"
 	"net"
+	"net/mail"
 	"net/smtp"
 	"net/textproto"
 	"net/url"
+	"strings"
 )
 
 ///////////////////////////////////////////////////////////////////////
@@ -235,4 +239,285 @@ func EncryptMailMessage(key, body []byte) ([]byte, error) {
 			"Content-Disposition: inline;\n filename=\"encrypted.asc\"\n\n" +
 			string(out.Bytes()) + "\n--" + bndry + "--")
 	return msg.Bytes(), nil
+}
+
+//---------------------------------------------------------------------
+/*
+ * Result type for parsing mail messages
+ */
+type MailContent struct {
+	Mode int    // message type (MDOE_XXX)
+	From string // sender email address
+	Body string // message body
+	Key  []byte // attached key or signing key (public)
+}
+
+/*
+ * Callback function to request user information:
+ * @param key int - info type requested (INFO_XXX)
+ * @param data string - additional data
+ * @return interface{} - return object
+ */
+type MailUserInfo func(key int, data string) interface{}
+
+//---------------------------------------------------------------------
+/*
+ * Parsing-related constants
+ */
+const (
+	ct_PLAIN   = "text/plain;"
+	ct_MP_MIX  = "multipart/mixed;"
+	ct_MP_ENC  = "multipart/encrypted;"
+	ct_MP_SIGN = "multipart/signed;"
+
+	MODE_PLAIN = iota
+	MODE_SIGN
+	MODE_ENC
+	MODE_SIGN_ENC
+	MODE_USIGN_ENC // unverified signature (key missing)
+
+	INFO_IDENTITY = iota
+	INFO_PASSPHRASE
+	INFO_SENDER
+)
+
+//---------------------------------------------------------------------
+/*
+ * Parse mail message:
+ * @param msg io.Reader - message reader
+ * @param getInfo MailUserInfo - callback for info retrieval
+ * @return error - error instance or nil
+ */
+func ParseMailMessage(msg io.Reader, getInfo MailUserInfo) (*MailContent, error) {
+	m, err := mail.ReadMessage(msg)
+	if err != nil {
+		return nil, err
+	}
+	addr, err := mail.ParseAddress(m.Header.Get("From"))
+	if err != nil {
+		return nil, err
+	}
+	ct := m.Header.Get("Content-Type")
+	var mc *MailContent = nil
+	if strings.HasPrefix(ct, ct_PLAIN) {
+		mc = new(MailContent)
+		mc.Mode = MODE_PLAIN
+		mc.Key = nil
+		data, err := ioutil.ReadAll(m.Body)
+		if err != nil {
+			return nil, err
+		}
+		mc.Body = string(data)
+	} else if strings.HasPrefix(ct, ct_MP_MIX) {
+		mc, err = ParsePlain(ct, m.Body)
+	} else if strings.HasPrefix(ct, ct_MP_ENC) {
+		mc, err = ParseEncrypted(ct, addr.Address, getInfo, m.Body)
+	} else if strings.HasPrefix(ct, ct_MP_SIGN) {
+		mc, err = ParseSigned(ct, addr.Address, m.Body)
+	}
+	if err != nil {
+		return nil, err
+	}
+	if mc == nil {
+		return nil, errors.New("Unparsed mail message")
+	}
+	mc.From = addr.Address
+	return mc, nil
+}
+
+//---------------------------------------------------------------------
+/*
+ * Parse plain text message.
+ * @param ct string - content type string
+ * @param body io.Reader - content reader
+ * @return *MailContent - parse result
+ * @return error - error instance or nil
+ */
+func ParsePlain(ct string, body io.Reader) (*MailContent, error) {
+	mc := new(MailContent)
+	mc.Mode = MODE_PLAIN
+	boundary := extractValue(ct, "boundary")
+	rdr := multipart.NewReader(body, boundary)
+	for {
+		if part, err := rdr.NextPart(); err == nil {
+			ct = part.Header.Get("Content-Type")
+			switch {
+			case strings.HasPrefix(ct, "text/plain;"):
+				data, err := ioutil.ReadAll(part)
+				if err != nil {
+					return nil, err
+				}
+				mc.Body = string(data)
+			case strings.HasPrefix(ct, "application/pgp-keys;"):
+				mc.Key, err = ioutil.ReadAll(part)
+				if err != nil {
+					return nil, err
+				}
+			default:
+				return nil, errors.New("Unhandled MIME part: " + ct)
+			}
+		} else if err == io.EOF {
+			break
+		} else {
+			return nil, err
+		}
+	}
+	return mc, nil
+}
+
+//---------------------------------------------------------------------
+/*
+ * Parse encrypted (and possibly signed) message.
+ * @param ct string - content type string
+ * @param addr string - sender address
+ * @param getInfo MailUserInfo - callback for info retrieval
+ * @param body io.Reader - content reader
+ * @return *MailContent - parse result
+ * @return error - error instance or nil
+ */
+func ParseEncrypted(ct, addr string, getInfo MailUserInfo, body io.Reader) (*MailContent, error) {
+	mc := new(MailContent)
+	mc.Mode = MODE_ENC
+	boundary := extractValue(ct, "boundary")
+	rdr := multipart.NewReader(body, boundary)
+	for {
+		if part, err := rdr.NextPart(); err == nil {
+			ct = part.Header.Get("Content-Type")
+			switch {
+			case strings.HasPrefix(ct, "application/pgp-encrypted"):
+				buf, err := ioutil.ReadAll(part)
+				if err != nil {
+					return nil, err
+				}
+				logger.Printf(logger.DBG, "application/pgp-encrypted: '%s'\n", strings.TrimSpace(string(buf)))
+				continue
+			case strings.HasPrefix(ct, "application/octet-stream;"):
+				rdr, err := armor.Decode(part)
+				if err != nil {
+					return nil, err
+				}
+				pw := getInfo(INFO_PASSPHRASE, "").(string)
+				prompt := func(keys []openpgp.Key, symmetric bool) ([]byte, error) {
+					priv := keys[0].PrivateKey
+					if priv.Encrypted {
+						priv.Decrypt([]byte(pw))
+					}
+					buf := new(bytes.Buffer)
+					priv.Serialize(buf)
+					return buf.Bytes(), nil
+				}
+				identity := getInfo(INFO_IDENTITY, "").(*openpgp.Entity)
+				md, err := openpgp.ReadMessage(rdr.Body, openpgp.EntityList{identity}, prompt, nil)
+				if err != nil {
+					return nil, err
+				}
+				if md.IsSigned {
+					mc.Mode = MODE_SIGN_ENC
+					id := getInfo(INFO_SENDER, addr).(*openpgp.Entity)
+					if id == nil {
+						mc.Mode = MODE_USIGN_ENC
+						content, err := ioutil.ReadAll(md.UnverifiedBody)
+						if err != nil {
+							return nil, err
+						}
+						mc.Body = string(content)
+						continue
+					}
+					md.SignedBy = crypto.GetKeyFromIdentity(id, crypto.KEY_SIGN)
+					md.SignedByKeyId = md.SignedBy.PublicKey.KeyId
+					mc.Key, err = crypto.GetArmoredPublicKey(id)
+					if err != nil {
+						return nil, err
+					}
+					content, err := ioutil.ReadAll(md.UnverifiedBody)
+					if err != nil {
+						return nil, err
+					}
+					if md.SignatureError != nil {
+						return nil, md.SignatureError
+					}
+					logger.Println(logger.INFO, "Signature verified OK")
+
+					m, err := mail.ReadMessage(bytes.NewBuffer(content))
+					if err != nil {
+						return nil, err
+					}
+					ct = m.Header.Get("Content-Type")
+					mc2, err := ParsePlain(ct, m.Body)
+					if err != nil {
+						return nil, err
+					}
+					mc.Body = mc2.Body
+				}
+			default:
+				return nil, errors.New("Unhandled MIME part: " + ct)
+			}
+		} else if err == io.EOF {
+			break
+		} else {
+			return nil, err
+		}
+	}
+	return mc, nil
+}
+
+//---------------------------------------------------------------------
+/*
+ * Parse signed unencrypted message.
+ * @param ct string - content type string
+ * @param addr string - sender address
+ * @param body io.Reader - content reader
+ * @return *MailContent - parse result
+ * @return error - error instance or nil
+ */
+func ParseSigned(ct, addr string, body io.Reader) (*MailContent, error) {
+	mc := new(MailContent)
+	mc.Mode = MODE_ENC
+	boundary := extractValue(ct, "boundary")
+	rdr := multipart.NewReader(body, boundary)
+	for {
+		if part, err := rdr.NextPart(); err == nil {
+			ct = part.Header.Get("Content-Type")
+			switch {
+			case strings.HasPrefix(ct, "text/plain;"):
+				data, err := ioutil.ReadAll(part)
+				if err != nil {
+					return nil, err
+				}
+				mc.Body = string(data)
+			case strings.HasPrefix(ct, "application/pgp-signature;"):
+				data, err := ioutil.ReadAll(part)
+				if err != nil {
+					return nil, err
+				}
+				fmt.Println("Signature: " + string(data))
+			}
+		} else if err == io.EOF {
+			break
+		} else {
+			return nil, err
+		}
+	}
+	return mc, nil
+}
+
+//---------------------------------------------------------------------
+/*
+ * Extract value from string ('... key="value" ...')
+ * @param s string - input string
+ * @param key string - name of key
+ * @param string - value string (or empty)
+ */
+func extractValue(s, key string) string {
+	idx := strings.Index(s, key)
+	skip := idx + len(key) + 2
+	if idx < 0 || len(s) < skip {
+		return ""
+	}
+	s = s[skip:]
+	idx = strings.IndexRune(s, '"')
+	if idx < 0 {
+		return ""
+	}
+	return s[:idx]
 }
