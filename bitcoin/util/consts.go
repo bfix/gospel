e621e8861716addea28547d440a46a0f9c575ec2
Bernd Fix <brf@hoi-polloi.org>
2017-04-19 21:32:10 +0200

    Renamed Genesis hash constant.

--- bitcoin/util/consts.go
+++ bitcoin/util/consts.go
@@ -1,6 +1,6 @@
 package util
 
 const (
-	// GENESIS_HASH is the hash value of the very first block in the blockchain.
-	GENESIS_HASH = "0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
+	// GenesisHash is the hash value of the very first block in the blockchain.
+	GenesisHash = "0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
 )
