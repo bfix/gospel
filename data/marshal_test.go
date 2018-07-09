ea067f2a038957ab829200b891907e963b9963bd
Bernd Fix <brf@hoi-polloi.org>
2018-07-09 02:24:42 +0200

    Added binary marshal/unmarshal functions for Golang objects.

---
+++ data/marshal_test.go
@@ -0,0 +1,64 @@
+package data
+
+import (
+	"encoding/hex"
+	"fmt"
+	"testing"
+)
+
+type NestedStruct struct {
+	A int64 `order:"big"`
+	B int32
+}
+
+func (n *NestedStruct) String() string {
+	return fmt.Sprintf("%v", *n)
+}
+
+type SubStruct struct {
+	G int32
+}
+
+func (s *SubStruct) String() string {
+	return fmt.Sprintf("%v", *s)
+}
+
+type MainStruct struct {
+	C uint64 `order:"big"`
+	D string
+	F *SubStruct
+	E []*NestedStruct
+}
+
+func TestNested(t *testing.T) {
+	r := new(MainStruct)
+	r.C = 19031962
+	r.D = "Just a test"
+	r.E = make([]*NestedStruct, 3)
+	r.F = new(SubStruct)
+	r.F.G = 0x23
+	for i := 0; i < 3; i++ {
+		n := new(NestedStruct)
+		n.A = int64(255 - i)
+		n.B = int32(815 * (i + 1))
+		r.E[i] = n
+	}
+
+	data, err := Marshal(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("<<< %v\n", r)
+	fmt.Printf("    [%s]\n", hex.EncodeToString(data))
+
+	s := new(MainStruct)
+	s.F = new(SubStruct)
+	s.E = make([]*NestedStruct, 3)
+	for i := 0; i < 3; i++ {
+		s.E[i] = new(NestedStruct)
+	}
+	if err = Unmarshal(s, data); err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf(">>> %v\n", s)
+}
