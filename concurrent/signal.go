dfea378fbce0ca5db95adbf11462751633adf776
Bernd Fix <brf@hoi-polloi.org>
2020-06-17 13:59:48 +0200

    Changed license to GNU AGPL v3.

--- concurrent/signal.go
+++ concurrent/signal.go
@@ -4,18 +4,20 @@ package concurrent
 // This file is part of Gospel.
 // Copyright (C) 2011-2020 Bernd Fix
 //
-// Gospel is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// Gospel is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
 //
-// Gospel is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// Gospel is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with Gospel.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: AGPL3.0-or-later
 //----------------------------------------------------------------------
 
 import (
--- network/doc.go
+++ network/doc.go
@@ -1,24 +1,28 @@
 package network
 
+//----------------------------------------------------------------------
+// This file is part of Gospel.
+// Copyright (C) 2011-2020 Bernd Fix
+//
+// Gospel is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// Gospel is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: AGPL3.0-or-later
+//----------------------------------------------------------------------
 /*
  * --------------------------------------------------------------------
  * Network-related functionality:
  *     - TCP/UDP network service with user-defined session handler
  *     - TOR (The Onion Router) queries and status
  * --------------------------------------------------------------------
- *
- * (c) 2010-2019 Bernd Fix   >Y<
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at
- * your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
