a5523cb7a53866cc34526f25353c971e942c2ad4
Bernd Fix <brf@hoi-polloi.org>
2021-03-08 18:01:34 +0100

    Added Tor tools folder.

---
+++ network/tor/tools/TorAuthCookie/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	// "/run/tor/control.authcookie" base64-encoded
+	enc := "L3J1bi90b3IvY29udHJvbC5hdXRoY29va2ll"
+	fname, err := base64.StdEncoding.DecodeString(enc)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	file, err := os.Open(string(fname))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(hex.EncodeToString(data))
+	os.Exit(0)
+}
