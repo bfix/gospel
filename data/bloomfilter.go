287d7b7664217c3e45c1c70e316a211a97d58ab9
Bernd Fix <brf@hoi-polloi.org>
2022-11-18 16:04:32 +0100

    bloomfilter: counting filter implemented.

--- data/bloomfilter.go
+++ data/bloomfilter.go
@@ -28,6 +28,39 @@ import (
 	"math/big"
 )
 
+// BloomFilterBase for custom bloom filter implementations
+// (e.g. simple, salted, counting, ...)
+type BloomFilterBase struct {
+	NumBits    uint32 `size:"big" json:"numBits"` // number of bits in filter
+	NumIdx     uint8  `size:"big" json:"numIdx"`  // number of indices
+	NumIdxBits uint8  `json:"numIdxBits"`         // number of bits per index
+	NumHash    uint8  `json:"numHash"`            // number of SHA256 hashes needed
+}
+
+// Helper method to extract the list of indices for an entry.
+func (bf *BloomFilterBase) indexList(entry []byte) []int {
+	totalIdx := make([]byte, 0)
+	hasher := sha256.New()
+	var i uint8
+	for i = 0; i < bf.NumHash; i++ {
+		hasher.Write(entry)
+		totalIdx = hasher.Sum(totalIdx)
+	}
+	v := new(big.Int).SetBytes(totalIdx)
+	mask := big.NewInt((1 << uint(bf.NumIdxBits)) - 1)
+	list := make([]int, bf.NumIdx)
+	for i = 0; i < bf.NumIdx; i++ {
+		j := new(big.Int).And(v, mask)
+		list[i] = int(j.Int64()) % int(bf.NumBits)
+		v = new(big.Int).Rsh(v, uint(bf.NumIdxBits))
+	}
+	return list
+}
+
+//----------------------------------------------------------------------
+// Generic bloomfilter
+//----------------------------------------------------------------------
+
 // A BloomFilter is a space/time efficient set of unique entries.
 // It can not enumerate its elements, but can check if an entry is contained
 // in the set. The check always succeeds for a contained entry, but can create
@@ -36,11 +69,9 @@ import (
 // generated for an entry, a BloomFilter can handle a given number of entries
 // with a desired upper-bound for the false-positive rate.
 type BloomFilter struct {
-	NumBits    uint32 `size:"big" json:"numBits"`     // number of bits in filter
-	NumIdx     uint8  `size:"big" json:"numIdx"`      // number of indices
-	NumIdxBits uint8  `json:"numIdxBits"`             // number of bits per index
-	NumHash    uint8  `json:"numHash"`                // number of SHA256 hashes needed
-	Bits       []byte `size:"(BitsSize)" json:"bits"` // bit storage
+	BloomFilterBase
+
+	Bits []byte `size:"(BitsSize)" json:"bits"` // bit storage
 }
 
 // NewBloomFilterDirect creates a new BloomFilter based on the number of bits
@@ -48,11 +79,13 @@ type BloomFilter struct {
 func NewBloomFilterDirect(numBits, numIdx int) *BloomFilter {
 	numIdxBits := int(math.Ceil(math.Log2(float64(numBits))))
 	return &BloomFilter{
-		NumBits:    uint32(numBits),
-		NumIdx:     uint8(numIdx),
-		NumIdxBits: uint8(numIdxBits),
-		NumHash:    uint8((numIdxBits*numIdx + 255) / 256),
-		Bits:       make([]byte, (numBits+7)/8),
+		BloomFilterBase: BloomFilterBase{
+			NumBits:    uint32(numBits),
+			NumIdx:     uint8(numIdx),
+			NumIdxBits: uint8(numIdxBits),
+			NumHash:    uint8((numIdxBits*numIdx + 255) / 256),
+		},
+		Bits: make([]byte, (numBits+7)/8),
 	}
 }
 
@@ -88,7 +121,7 @@ func (bf *BloomFilter) SameKind(bf2 *BloomFilter) bool {
 // Add an entry to the BloomFilter.
 func (bf *BloomFilter) Add(entry []byte) {
 	for _, idx := range bf.indexList(entry) {
-		pos, mask := resolve(idx)
+		pos, mask := bf.resolve(idx)
 		bf.Bits[pos] |= mask
 	}
 }
@@ -99,11 +132,13 @@ func (bf *BloomFilter) Combine(bf2 *BloomFilter) *BloomFilter {
 		return nil
 	}
 	res := &BloomFilter{
-		NumBits:    bf.NumBits,
-		NumIdx:     bf.NumIdx,
-		NumIdxBits: bf.NumIdxBits,
-		NumHash:    bf.NumHash,
-		Bits:       make([]byte, len(bf.Bits)),
+		BloomFilterBase: BloomFilterBase{
+			NumBits:    bf.NumBits,
+			NumIdx:     bf.NumIdx,
+			NumIdxBits: bf.NumIdxBits,
+			NumHash:    bf.NumHash,
+		},
+		Bits: make([]byte, len(bf.Bits)),
 	}
 	for i := range res.Bits {
 		res.Bits[i] = bf.Bits[i] | bf2.Bits[i]
@@ -117,7 +152,7 @@ func (bf *BloomFilter) Combine(bf2 *BloomFilter) *BloomFilter {
 // ("false-positives").
 func (bf *BloomFilter) Contains(entry []byte) bool {
 	for _, idx := range bf.indexList(entry) {
-		pos, mask := resolve(idx)
+		pos, mask := bf.resolve(idx)
 		if (bf.Bits[pos] & mask) == 0 {
 			return false
 		}
@@ -125,32 +160,14 @@ func (bf *BloomFilter) Contains(entry []byte) bool {
 	return true
 }
 
-// Helper method to extract the list of indices for an entry.
-func (bf *BloomFilter) indexList(entry []byte) []int {
-	totalIdx := make([]byte, 0)
-	hasher := sha256.New()
-	var i uint8
-	for i = 0; i < bf.NumHash; i++ {
-		hasher.Write(entry)
-		totalIdx = hasher.Sum(totalIdx)
-	}
-	v := new(big.Int).SetBytes(totalIdx)
-	mask := big.NewInt((1 << uint(bf.NumIdxBits)) - 1)
-	list := make([]int, bf.NumIdx)
-	for i = 0; i < bf.NumIdx; i++ {
-		j := new(big.Int).And(v, mask)
-		list[i] = int(j.Int64()) % int(bf.NumBits)
-		v = new(big.Int).Rsh(v, uint(bf.NumIdxBits))
-	}
-	return list
-}
-
 // Helper method to resolve an index into byte/bit positions in the data
 // of the BloomFilter.
-func resolve(idx int) (int, byte) {
+func (bf *BloomFilter) resolve(idx int) (int, byte) {
 	return idx >> 3, byte(1 << uint(idx&7))
 }
 
+//----------------------------------------------------------------------
+// Salted bloomfilter
 //----------------------------------------------------------------------
 
 // SaltedBloomFilter is a bloom filter where each entr is "salted" with
@@ -229,4 +246,111 @@ func (bf *SaltedBloomFilter) Combine(bf2 *SaltedBloomFilter) *SaltedBloomFilter 
 // ("false-positives").
 func (bf *SaltedBloomFilter) Contains(entry []byte) bool {
 	return bf.BloomFilter.Contains(bf.saltEntry(entry))
+}
+
+//----------------------------------------------------------------------
+// Counting bloomfilter
+//----------------------------------------------------------------------
+
+// CountingBloomFilter is an extension of a generic bloomfilter that
+// keeps a count instead of a single bit for masking. This allows the
+// deletion of entries for the cost of 32x emory increase.
+type CountingBloomFilter struct {
+	BloomFilterBase
+
+	Counts []uint32 `size:"(NumBits)" json:"bits"` // counter storage
+}
+
+// NewCoutingBloomFilterDirect creates a new BloomFilter based on the
+// number of bits in the filter and the number of indices to be used.
+func NewCountingBloomFilterDirect(numBits, numIdx int) *CountingBloomFilter {
+	numIdxBits := int(math.Ceil(math.Log2(float64(numBits))))
+	return &CountingBloomFilter{
+		BloomFilterBase: BloomFilterBase{
+			NumBits:    uint32(numBits),
+			NumIdx:     uint8(numIdx),
+			NumIdxBits: uint8(numIdxBits),
+			NumHash:    uint8((numIdxBits*numIdx + 255) / 256),
+		},
+		Counts: make([]uint32, numBits),
+	}
+}
+
+// NewCoutingBloomFilter creates a new BloomFilter based on the upper-bounds
+// for the number of entries and the "false-positive" rate.
+func NewCountingBloomFilter(numExpected int, falsePositiveRate float64) *CountingBloomFilter {
+	// do some math and calculate the number of indices and number of bits
+	// in the new BloomFilter given an upper-bound for the number of entries
+	// and the "false-positive" rate.
+	numIdx := int(math.Ceil(-math.Log2(falsePositiveRate)))
+	numBits := int(float64(numIdx*numExpected) / math.Ln2)
+	return NewCountingBloomFilterDirect(numBits, numIdx)
+}
+
+// Size returns the size of the binary representation
+func (bf *CountingBloomFilter) Size() uint {
+	return uint(7 + 4*bf.NumBits)
+}
+
+// Add an entry to the BloomFilter.
+func (bf *CountingBloomFilter) Add(entry []byte) {
+	for _, idx := range bf.indexList(entry) {
+		bf.Counts[idx] += 1
+	}
+}
+
+// SameKind checks if two BloomFilter have the same parameters.
+func (bf *CountingBloomFilter) SameKind(bf2 *CountingBloomFilter) bool {
+	return bf.NumBits == bf2.NumBits &&
+		bf.NumHash == bf2.NumHash &&
+		bf.NumIdx == bf2.NumIdx &&
+		bf.NumIdxBits == bf2.NumIdxBits
+}
+
+// Combine merges two BloomFilters (of same kind) into a new one.
+func (bf *CountingBloomFilter) Combine(bf2 *CountingBloomFilter) *CountingBloomFilter {
+	if !bf.SameKind(bf2) {
+		return nil
+	}
+	res := &CountingBloomFilter{
+		BloomFilterBase: BloomFilterBase{
+			NumBits:    bf.NumBits,
+			NumIdx:     bf.NumIdx,
+			NumIdxBits: bf.NumIdxBits,
+			NumHash:    bf.NumHash,
+		},
+		Counts: make([]uint32, bf.NumBits),
+	}
+	for i := range res.Counts {
+		res.Counts[i] = bf.Counts[i] + bf2.Counts[i]
+	}
+	return res
+}
+
+// Contains returns true if the BloomFilter contains the given entry, and
+// false otherwise. If an entry was added to the set, this function will
+// always return 'true'. It can return 'true' for entries not in the set
+// ("false-positives").
+func (bf *CountingBloomFilter) Contains(entry []byte) bool {
+	for _, idx := range bf.indexList(entry) {
+		if bf.Counts[idx] == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// Remove an entry from the bloomfilter
+func (bf *CountingBloomFilter) Remove(entry []byte) bool {
+	// make sure the entry is stored in the filter
+	idxList := bf.indexList(entry)
+	for _, idx := range idxList {
+		if bf.Counts[idx] == 0 {
+			return false
+		}
+	}
+	for _, idx := range idxList {
+		bf.Counts[idx]--
+	}
+	return true
 }
--- data/bloomfilter_test.go
+++ data/bloomfilter_test.go
@@ -164,3 +164,79 @@ func TestSaltedBloomfilter(t *testing.T) {
 		t.Logf("false-positive rate %f > %f", fpReal, fpRate)
 	}
 }
+
+func TestCountingBloomfilter(t *testing.T) {
+
+	n := 500
+	fpRate := 0.0001
+
+	// generate positives (entries in the set)
+	positives := make(EntryList, n)
+	for i := 0; i < n; i++ {
+		data := make(Entry, 32)
+		if _, err := rand.Read(data); err != nil {
+			t.Fatal(err)
+		}
+		positives[i] = data
+	}
+	sort.Sort(positives)
+
+	// generate negatives (entries outside the set)
+	negatives := make(EntryList, n)
+	for i := 0; i < n; {
+		data := make(Entry, 32)
+		if _, err := rand.Read(data); err != nil {
+			t.Fatal(err)
+		}
+		if !positives.Contains(data) {
+			negatives[i] = data
+			i++
+		}
+	}
+
+	// create BloomFilter
+	bf := NewCountingBloomFilter(n, fpRate)
+
+	// add positives to bloomfilter
+	for _, e := range positives {
+		bf.Add(e)
+	}
+
+	// check lookup of positives
+	count := 0
+	for _, e := range positives {
+		if !bf.Contains(e) {
+			count++
+		}
+	}
+	if count > 0 {
+		t.Fatalf("FAILED with %d false-negatives", count)
+	}
+
+	// check lookup of negatives
+	count = 0
+	for _, e := range negatives {
+		if bf.Contains(e) {
+			count++
+		}
+	}
+	fpReal := float64(count) / float64(n)
+	if fpReal > fpRate {
+		t.Logf("false-positive rate %f > %f", fpReal, fpRate)
+	}
+
+	// remove entries...
+	for _, e := range positives {
+		if !bf.Remove(e) {
+			t.Fatal("failed to remove entry")
+		}
+	}
+	// check for empty filter
+	var sum uint32
+	for _, count := range bf.Counts {
+		sum += count
+	}
+	if sum != 0 {
+		t.Fatal("bf not empty")
+	}
+}
