6fc9910ae17d6aa9d7e5a87afba6d9fe965325ce
Bernd Fix <brf@hoi-polloi.org>
2023-07-14 15:37:12 +0200

    Show prv/pub for HD and standard accounts.

--- bitcoin/tools/passphrase2seed/main.go
+++ bitcoin/tools/passphrase2seed/main.go
@@ -2,7 +2,7 @@ package main
 
 //----------------------------------------------------------------------
 // This file is part of Gospel.
-// Copyright (C) 2011-2021 Bernd Fix  >Y<
+// Copyright (C) 2011-2023 Bernd Fix  >Y<
 //
 // Gospel is free software: you can redistribute it and/or modify it
 // under the terms of the GNU Affero General Public License as published
@@ -32,26 +32,74 @@ import (
 
 func main() {
 
-	fmt.Printf(">>> Passphrase: ")
+	fmt.Printf(">>> Passphrase (Entropy): ")
 	rdr := bufio.NewReader(os.Stdin)
-	in, _, err := rdr.ReadLine()
+	pp1, _, err := rdr.ReadLine()
 	if err != nil {
 		fmt.Println("<<< ERROR: " + err.Error())
 		return
 	}
-	ent := sha256.Sum256(in)
+	ent := sha256.Sum256(pp1)
 	words, err := wallet.EntropyToWords(ent[:])
 	if err != nil {
 		fmt.Println("<<< ERROR: " + err.Error())
 		return
 	}
-	seed, _ := wallet.WordsToSeed(words, "")
+	fmt.Printf(">>>    Password (BIP 39): ")
+	pp2, _, err := rdr.ReadLine()
+	if err != nil {
+		fmt.Println("<<< ERROR: " + err.Error())
+		return
+	}
+	seed, _ := wallet.WordsToSeed(words, string(pp2))
 
-	fmt.Printf("<<<    Entropy: %s\n", hex.EncodeToString(ent[:]))
-	fmt.Printf("<<<       Seed: %s\n", hex.EncodeToString(seed))
-	fmt.Println("<<< Seed words:")
+	fmt.Printf("<<<     Entropy: %s\n", hex.EncodeToString(ent[:]))
+	fmt.Printf("<<< BIP-32 seed: %s\n", hex.EncodeToString(seed))
+	fmt.Println("<<< BIP-39 seed:")
 	n := len(words) / 2
 	for i := 0; i < n; i++ {
 		fmt.Printf("<<<    %2d: %-20s %2d: %-20s\n", i+1, words[i], i+n+1, words[i+n])
+	}
+
+	hd, err := wallet.NewHD(seed)
+	if err != nil {
+		fmt.Println("<<< ERROR: " + err.Error())
+		return
+	}
+	pk := hd.MasterPublic()
+	fmt.Printf("<<< BIP-32 Master/Root Pub: %s\n", pk)
+	sk := hd.MasterPrivate()
+	fmt.Printf("<<< BIP-32 Master/Root Prv: %s\n", sk)
+
+	type task struct {
+		id   string
+		name string
+		path string
+	}
+	var tasks = []task{
+		{"x", "BIP-44", "m/44'/0'/0'"},
+		{"y", "BIP-49", "m/49'/0'/0'"},
+		{"z", "BIP-84", "m/84'/0'/0'"},
+	}
+
+	for _, t := range tasks {
+		// create a HD wallet for the given seed
+		fmt.Printf("<<< %s\n", t.name)
+		vc := wallet.VersionCodes[t.id]
+		bsk, err := hd.Private(t.path)
+		if err != nil {
+			fmt.Println("<<< ERROR: " + err.Error())
+			continue
+		}
+		bsk.Data.Version = vc.Private
+		fmt.Printf("<<<     %s\n", bsk)
+
+		bpk, err := hd.Public(t.path)
+		if err != nil {
+			fmt.Println("<<< ERROR: " + err.Error())
+			continue
+		}
+		bpk.Data.Version = vc.Public
+		fmt.Printf("<<<     %s\n", bpk)
 	}
 }
